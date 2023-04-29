8ec6657ac4912029aefa45170ae37f0302c420ed
evan <evan@local>
2023-04-28 19:11:19 -0700

    Basic WASD movement on a cube

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public CharacterController characterController;
+    public PlayerControls playerInput;
+
+    private Vector3 moveInput = Vector3.zero;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerInput = new();
+        playerInput.CharacterControls.Enable();
+    }
+
+    private void OnEnable()
+    {
+        playerInput.CharacterControls.Movement.performed += MovementPerformed;
+        playerInput.CharacterControls.Movement.canceled += MovementCanceled;
+    }
+
+    private void OnDisable()
+    {
+        playerInput.CharacterControls.Movement.performed -= MovementPerformed;
+        playerInput.CharacterControls.Movement.canceled -= MovementCanceled;
+    }
+
+    private void MovementCanceled(InputAction.CallbackContext obj)
+    {
+        moveInput = Vector3.zero;
+    }
+
+    private void MovementPerformed(InputAction.CallbackContext obj)
+    {
+        var inputVec = obj.ReadValue<Vector2>();
+        moveInput = new Vector3(inputVec.x, 0, inputVec.y);
+    }
+
+    private void Update()
+    {
+        if (moveInput != Vector3.zero)
+            characterController.Move(moveInput * (moveSpeed * Time.deltaTime));
+    }
+}
