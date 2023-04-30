d723bf1ef4664c11eb82f1a3994877cde6a5a57c
evan <evan@local>
2023-04-29 21:03:16 -0700

    Producer timer bar still has bugs

--- Assets/Scripts/Producer.cs
+++ Assets/Scripts/Producer.cs
@@ -1,25 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class Producer : AreaOfInterest
 {
     public Animator bodyAnimator;
-    public Image sliderFill;
+    public Slider timerUI;
+    public Image timerFill;
     private static readonly int IsProducing = Animator.StringToHash("isProducing");
 
     public override void SetPackageType(PackageType packageType)
     {
         base.SetPackageType(packageType);
         spriteRenderer.color = packageType.color;
-        sliderFill.color = packageType.color;
+        timerFill.color = packageType.color;
     }
     
     public void InstantlyCreatePackage(PackageType packageType)
     {
         SetPackageType(packageType);
         gameObject.SetActive(true);
+        timerUI.enabled = false;
         bodyAnimator.SetBool(IsProducing, false);
     }
 
@@ -27,6 +30,7 @@ public class Producer : AreaOfInterest
     {
         SetPackageType(packageType);
         gameObject.SetActive(true);
+        timerUI.enabled = true;
         bodyAnimator.SetBool(IsProducing, true);
         StartTimer(creationTime);
     }
@@ -34,6 +38,7 @@ public class Producer : AreaOfInterest
     protected override void OnTimerDone()
     {
         bodyAnimator.SetBool(IsProducing, false);
+        timerUI.enabled = false;
         base.OnTimerDone();
     }
 }
