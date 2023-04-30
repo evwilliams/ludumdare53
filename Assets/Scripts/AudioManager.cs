4e4d5f93eff72b4c5b15fbc944e387a427483cdb
evan <evan@local>
2023-04-30 14:17:44 -0700

    Play producer sounds

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,19 +4,37 @@ public class AudioManager : MonoBehaviour
 {
     public AudioSource audioPlayer;
     public DestinationChannel destinationChannel;
+    public ProducerChannel producerChannel;
+
+    public AudioClip clip_ProducerStarted;
+    public AudioClip clip_ProducerCompleted;
     
     private void OnEnable()
     {   
         destinationChannel.ResponseDisplayed += ResponseDisplayed;
+        producerChannel.ProducerStarted += ProducerStarted;
+        producerChannel.ProducerCompleted += ProducerCompleted;
     }
-
+    
     private void OnDisable()
     {
         destinationChannel.ResponseDisplayed -= ResponseDisplayed;
+        producerChannel.ProducerStarted -= ProducerStarted;
+        producerChannel.ProducerCompleted -= ProducerCompleted;
     }
 
     private void ResponseDisplayed(Response response)
     {
         audioPlayer.PlayOneShot(response.audioClip);
+    }
+
+    private void ProducerStarted(Producer arg0)
+    {
+        audioPlayer.PlayOneShot(clip_ProducerStarted, 0.5f);
+    }
+
+    private void ProducerCompleted(Producer arg0)
+    {
+        audioPlayer.PlayOneShot(clip_ProducerCompleted, 0.5f);
     }
 }
